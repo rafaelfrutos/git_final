58f7ff53d767fb1e1db8005c3cc58a46dfcc1c6f
rafael <rafaelfrutos41@gmail.com>
2024-08-30 23:34:57 -0400

    cambiando texto

--- experimentar.py
+++ experimentar.py
@@ -4,6 +4,6 @@ pal1 = input("Palabra1: ")
 pal2 = input("Palabra2: ")
 pal3 = input("Palabra3: ")
 pal4 = input('Palabra 4: ')
-madlib = f"Estamos probando un {sus} para saber que tal se {adj}. Es muy importante saber\
+madlib = f"Estamos probando un {sus} para saber que tal se {adj}. Es muy kjjkjkkkimportante saber\
 cada {pal1} y de tekorei nomas estamos {pal2} y {pal3}, por eso vemos que estamos {pal4}."
 print(madlib)
