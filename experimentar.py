696f3f6cd55f609399f55d3899cda4ea30ffba40
rafael <rafaelfrutos41@gmail.com>
2024-08-30 20:54:01 -0400

    primer commit

---
+++ experimentar.py
@@ -0,0 +1,9 @@
+sus = input("Sustantivo: ")
+adj = input("Adjetivo: ")
+pal1 = input("Palabra1: ")
+pal2 = input("Palabra2: ")
+pal3 = input("Palabra3: ")
+pal4 = input('Palabra 4: ')
+madlib = f"Estamos probando un {sus} para saber que tal se {adj}. Es muy importante saber\
+cada {pal1} y de tekorei nomas estamos {pal2} y {pal3}, por eso vemos que estamos {pal4}."
+print(madlib)
