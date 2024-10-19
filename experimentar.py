c9a199a0e16c4c51313f3e3df010034d2fac5c05
unknown <aricampu97@gmail.com>
2024-10-19 18:48:32 -0400

    Alargue mas las palabras, tambien vos alarga mas por favor, entre mas largo mejor

--- experimentar.py
+++ experimentar.py
@@ -5,6 +5,6 @@ pal2 = input("Palabra2: ")
 pal3 = input("Palabra3: ")
 pal4 = input('Palabra 4: ')
 madlib = f"Estamos probando un {sus} para saber que tal me va y voy y se fue y yo vole de el se {adj}. Es muy importante saber\
-cada {pal1} y de tekorei nomas estamos {pal2} y {pal3}, por eso vemos que estamos {pal4}."
+cada {pal1} y de tekorei nomas estamos {pal2} y {pal3}, por eso vemos que estamos agregando mas cosas para alargar las palabras y oraciones {pal4}."
 print(madlib)
 #Comentando para anhadir nuevas caracterisitcas
